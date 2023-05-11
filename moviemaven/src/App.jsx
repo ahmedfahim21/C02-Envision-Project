5a28f65134b4966442be63a3260a2779bd9ab598
Naveen-Kakarla <naveenkakarla12@gmail.com>
2023-05-11 19:45:31 +0530

    footer section

--- moviemaven/src/App.jsx
+++ moviemaven/src/App.jsx
@@ -10,10 +10,22 @@ const App =() =>{
         <div>
             <Hero/>
             <Movies/>
-            <Footer/>
+            <Footer>
+            <h2 id="about-us">About us</h2>
+              <p>This is some information about our movie review and rating app.</p>
+
+             <h2 id="help">Help</h2>
+                 <p>If you need help using our app, please contact us at help@moviemaven.com.</p>
+
+             <h2 id="terms-and-conditions">Terms and conditions</h2>
+                 <p>By using our app, you agree to our terms and conditions.</p>
+
+             <h2 id="privacy-policy">Privacy policy</h2>
+                  <p>We take your privacy seriously. Please read our privacy policy for more information.</p>
+            </Footer>
             
         </div>
     );
 }
 
-export default App
+export default App;   
--- moviemaven/src/components/Footer.jsx
+++ moviemaven/src/components/Footer.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className=" font-[Roboto Slab] bg-background p-16 flex flex-col mx-auto">Footer</div>
-  )
+    <><div className=" font-[Roboto Slab] bg-background p-16 flex flex-col mx-auto">footer</div>
+      <footer>
+        <ul>
+          <li><a href="#about-us">About us</a></li>
+          <li><a href="#help">Help</a></li>
+          <li><a href="#terms-and-conditions">Terms and conditions</a></li>
+          <li><a href="#privacy-policy">Privacy policy</a></li>
+        </ul>
+     </footer>
+    </>
+    
+  );
 }
 
-export default Footer
+export default Footer;
